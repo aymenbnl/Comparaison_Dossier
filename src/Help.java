f75176e39ed79e8def7c76949d321c18f42a534c
halimachowiette <76652879+halima301@users.noreply.github.com>
2021-01-17 16:58:13 +0100

    Modification de l'interface "Help.java": Background d'interface Modification des noms des bouttons

--- src/Help.java
+++ src/Help.java
@@ -41,29 +41,29 @@ public class Help extends JDialog {
 		setTitle("About");
 		setBounds(100, 100, 583, 422);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBackground(new Color(255, 255, 255));
+		contentPanel.setBackground(new Color(255, 228, 225));
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Choisir votre r\u00E9pertoires");
+		JLabel lblNewLabel = new JLabel("Choisir des r\u00E9pertoires");
 		lblNewLabel.setFont(new Font("Segoe UI", Font.BOLD, 15));
-		lblNewLabel.setBounds(10, 11, 226, 14);
+		lblNewLabel.setBounds(20, 43, 226, 25);
 		contentPanel.add(lblNewLabel);
 		
 		JLabel lblcompareThem = new JLabel("Comparer");
 		lblcompareThem.setFont(new Font("Segoe UI", Font.BOLD, 15));
-		lblcompareThem.setBounds(10, 125, 125, 14);
+		lblcompareThem.setBounds(87, 154, 85, 35);
 		contentPanel.add(lblcompareThem);
 		
-		JLabel lblSelectSynchronizationSettings = new JLabel("Choisir votre param\u00E9tres");
+		JLabel lblSelectSynchronizationSettings = new JLabel("Param\u00E9tres");
 		lblSelectSynchronizationSettings.setFont(new Font("Segoe UI", Font.BOLD, 15));
-		lblSelectSynchronizationSettings.setBounds(10, 197, 301, 14);
+		lblSelectSynchronizationSettings.setBounds(87, 222, 101, 39);
 		contentPanel.add(lblSelectSynchronizationSettings);
 		
 		JLabel lblPressSynchronizeTo = new JLabel("Synchroniser");
 		lblPressSynchronizeTo.setFont(new Font("Segoe UI", Font.BOLD, 15));
-		lblPressSynchronizeTo.setBounds(10, 272, 274, 14);
+		lblPressSynchronizeTo.setBounds(87, 305, 109, 35);
 		contentPanel.add(lblPressSynchronizeTo);
 		
 		JLabel lblBasicUsage = new JLabel("");
@@ -71,47 +71,37 @@ public class Help extends JDialog {
 		lblBasicUsage.setBounds(10, 11, 85, 14);
 		contentPanel.add(lblBasicUsage);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_1.setBounds(10, 150, 186, 39);
-		contentPanel.add(panel_1);
-		panel_1.setLayout(new GridLayout(1, 0, 0, 0));
-		
-		JLabel label_1 = new JLabel("");
-		label_1.setBackground(new Color(255, 255, 255));
-		label_1.setIcon(new ImageIcon(Help.class.getResource("/sync/icons/comp2.png")));
-		panel_1.add(label_1);
-		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_2.setBounds(10, 213, 47, 48);
+		panel_2.setBounds(276, 222, 47, 48);
 		contentPanel.add(panel_2);
 		panel_2.setLayout(new CardLayout(0, 0));
 		
 		JLabel label_2 = new JLabel("");
+		panel_2.add(label_2, "name_5296505302700");
 		label_2.setBackground(Color.GRAY);
 		label_2.setIcon(new ImageIcon(Help.class.getResource("/sync/icons/set_syn.png")));
-		panel_2.add(label_2, "name_11271244187530");
-		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_3.setBounds(10, 301, 186, 39);
-		contentPanel.add(panel_3);
-		panel_3.setLayout(new GridLayout(1, 0, 0, 0));
-		
-		JLabel label_3 = new JLabel("");
-		label_3.setBackground(new Color(255, 255, 255));
-		label_3.setIcon(new ImageIcon(Help.class.getResource("/sync/icons/syn2.png")));
-		panel_3.add(label_3);
 		
 		JLabel label = new JLabel("");
-		label.setBounds(10, 47, 531, 64);
+		label.setBounds(10, 79, 531, 64);
 		contentPanel.add(label);
 		label.setBackground(Color.WHITE);
 		label.setIcon(new ImageIcon(Help.class.getResource("/sync/icons/selectionner.png")));
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setBounds(206, 154, 182, 35);
+		contentPanel.add(label_1);
+		label_1.setBackground(Color.PINK);
+		label_1.setIcon(new ImageIcon(Help.class.getResource("/sync/icons/comp2.png")));
+		
+		JLabel label_3 = new JLabel("");
+		label_3.setBounds(206, 305, 182, 35);
+		contentPanel.add(label_3);
+		label_3.setBackground(Color.PINK);
+		label_3.setIcon(new ImageIcon(Help.class.getResource("/sync/icons/syn2.png")));
 		{
 			JPanel buttonPane = new JPanel();
-			buttonPane.setBackground(new Color(255, 255, 255));
+			buttonPane.setBackground(new Color(255, 228, 225));
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
